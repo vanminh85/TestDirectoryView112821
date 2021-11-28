a1f7f1f9edde0564f773a8839ae08b35489c434d
Nguyen Van Minh <minh.nguyen@katalon.com>
2021-11-28 16:30:48 +0700

    first commit

---
+++ Test Suites/NewTestSuite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>NewTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a167ed7-3d57-4dda-a873-3cf3d1984c0f</testSuiteGuid>
+   <testCaseLink>
+      <guid>2051f035-07a7-4b76-b617-3e5da903f5b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Folder.Test.Case/Folder1.Folder2.Folder3/Form1234</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
