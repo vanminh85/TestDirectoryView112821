50e7c12845d0a9eedff016c2217d6e9fd13c2652
Nguyen Van Minh <minh.nguyen@katalon.com>
2021-12-10 14:31:39 +0700

    first commit

--- Test Suites/TestSuiteCollection121021.ts
+++ Test Suites/TestSuiteCollection121021.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>Simple Test Suite Collection</name>
+   <name>TestSuiteCollection121021</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
@@ -13,7 +13,7 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/Simple Test Suite</testSuiteEntity>
+         <testSuiteEntity>Test Suites/TestSuite121021</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
